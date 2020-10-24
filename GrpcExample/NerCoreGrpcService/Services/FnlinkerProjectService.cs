df472000437fb0e02e8aa46500eeb1b2cc0359be
quqi <quqi@fnlinker.com>
2020-10-24 16:45:36 +0800

    添加Grpc服务

---
+++ GrpcExample/NerCoreGrpcService/Services/FnlinkerProjectService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using NerCoreGrpcService.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NerCoreGrpcService.Services
+{
+    public class FnlinkerProjectService : ProjectService.ProjectServiceBase
+    {
+        public override Task<AddProjectReponse> AddProjectAsync(AddProjectRequest request, ServerCallContext context)
+        {
+
+            return Task.FromResult(new AddProjectReponse
+            {
+                No = "1",
+                ProjectName = $"{request.ProjectName}_服务端"
+            });
+        }
+    }
+}
---
+++ GrpcExample/NetCoreGrpcClient/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NerCoreGrpcService.Protos;
+
+namespace NetCoreGrpcClient.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ProjectService.ProjectServiceClient(channel);
+            var res = await client.AddProjectAsyncAsync(new AddProjectRequest
+            {
+                ProjectName = "Test添加新项目"
+            });
+
+            return $"[{res.No}-{res.ProjectName}]";
+        }
+    }
+}
